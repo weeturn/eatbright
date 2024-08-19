9372ac69ef5bbf75cb8539fc3ca5088a09e97ec7
weeturn <sasquatch110@gmail.com>
2024-08-19 20:04:31 +0800

    modify manage.py

--- backend/manage.py
+++ backend/manage.py
@@ -3,8 +3,9 @@
 import os
 import sys
 
-
+sys.path.append(os.path.join(os.path.dirname(__file__), 'backend'))
 def main():
+    
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eatbright.settings')
     try:
